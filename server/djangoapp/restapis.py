887cf80a24c59e8fa1cf034a929d1af4ae0e6808
LizaLikhavitskaya <tarasova.a.liza@gmail.com>
2023-11-14 01:08:45 -0500

    Add changes, not working

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -1,11 +1,7 @@
 import requests
 import json
-from .models import CarDealer
+from .models import CarDealer, DealerReview 
 from requests.auth import HTTPBasicAuth
-
-# Create a `get_request` to make HTTP GET requests
-# e.g., response = requests.get(url, params=params, headers={'Content-Type': 'application/json'},
-#                                     auth=HTTPBasicAuth('apikey', api_key))
 
 def get_request(url, **kwargs):
     print(kwargs)
@@ -26,7 +22,6 @@ def get_request(url, **kwargs):
 # e.g., response = requests.post(url, params=kwargs, json=payload)
 
 
-# Create a get_dealers_from_cf method to get dealers from a cloud function
 def get_dealers_from_cf(url, **kwargs):
     results = []
     # Call get_request with a URL parameter
@@ -52,7 +47,41 @@ def get_dealers_from_cf(url, **kwargs):
 # - Call get_request() with specified arguments
 # - Parse JSON results into a DealerView object list
 
+def get_dealer_reviews_from_cf(url, dealerId): 
+    results = []
+    # Call get_request with a URL parameter
+    json_result = get_request(url)
+    if json_result:
+        reviews = json.loads(json_result[:len(json_result)])
+        for review in reviews:
+            review_doc = review
+            review_obj = DealerReview(dealership=review_doc["dealership"], name=review_doc["name"], purchase=review_doc["purchase"],
+                                   id=review_doc["id"], review=review_doc["review"], purchase_date=review_doc["purchase_date"],
+                                   car_make=review_doc["car_make"],
+                                   car_model=review_doc["car_model"], car_year=review_doc["car_year"])
+            results.append(review_obj)
 
+    return results
+
+
+
+"""
+def get_dealer_reviews_from_cf(url, dealerId): 
+    results = []
+    # Call get_request with a URL parameter
+    json_result = get_request(url)
+    if json_result:
+        reviews = json_result[:len(json_result)]
+        for review in reviews:
+            review_doc = review
+            review_obj = DealerReview(dealership=review_doc["dealership"], name=review_doc["name"], purchase=review_doc["purchase"],
+                                   id=review_doc["id"], review=review_doc["review"], purchase_date=review_doc["purchase_date"],
+                                   car_make=review_doc["car_make"],
+                                   car_model=review_doc["car_model"], car_year=review_doc["car_year"])
+            results.append(review_obj)
+
+    return results
+"""
 # Create an `analyze_review_sentiments` method to call Watson NLU and analyze text
 # def analyze_review_sentiments(text):
 # - Call get_request() with specified arguments
--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -4,6 +4,7 @@ from django.contrib.auth.models import User
 from django.shortcuts import get_object_or_404, render, redirect
 #from .models import CarDealer
 from .restapis import get_dealers_from_cf 
+from .restapis import get_dealer_reviews_from_cf
 from django.contrib.auth import login, logout, authenticate
 from django.contrib import messages
 from datetime import datetime
@@ -93,9 +94,14 @@ def get_dealerships(request):
 
 
 # Create a `get_dealer_details` view to render the reviews of a dealer
-# def get_dealer_details(request, dealer_id):
-# ...
-
+def get_dealer_details(request, dealer_id):
+    if request.method == "GET":
+        review_url = "https://lizavetalikh-5000.theiadocker-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai/api/get_reviews"
+        reviews = get_dealer_reviews_from_cf(review_url, dealer_id)
+        print(reviews)
+        context["reviews"] = reviews
+        
+        return render(request, 'djangoapp/dealer_details.html', context)
 # Create a `add_review` view to submit a review
 # def add_review(request, dealer_id):
 # ...
